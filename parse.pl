b76de76c10f5fc4f7fe6a2cc4ddb3e4ba8afc5a7
muntoo <muntoonxt@gmail.com>
2015-03-22 04:54:02 -0700

    Added more URLs

--- parse.pl
+++ parse.pl
@@ -5,10 +5,10 @@ use warnings;
 
 # Find => Replace
 my %data = (
-	"/*[[theme_drive]]*/"           => "domain(\"drive.google.com\"), url-prefix(\"http://docs.google.com/document/u/\"), url-prefix(\"https://docs.google.com/document/u/\")",
+	"/*[[theme_drive]]*/"           => "domain(\"drive.google.com\"), url-prefix(\"http://docs.google.com/document/u/\"), url-prefix(\"https://docs.google.com/document/u/\"), url-prefix(\"http://docs.google.com/spreadsheets/u/\"), url-prefix(\"https://docs.google.com/spreadsheets/u/\")",
 	"/*[[theme_docs]]*/"            => "url-prefix(\"http://docs.google.com/document/d/\"), url-prefix(\"https://docs.google.com/document/d/\")",
 	"/*[[theme_sheets]]*/"          => "url-prefix(\"http://docs.google.com/spreadsheets/d/\"), url-prefix(\"https://docs.google.com/spreadsheets/d/\")",
-	"/*[[theme_sheets_canvas]]*/"   => "url-prefix(\"http://docs.google.com/spreadsheets/d/\"), url-prefix(\"https://docs.google.com/spreadsheets/d/\")",
+	"/*[[theme_sheets_canvas]]*/"   => "url-prefix(\"http://docs.google.com/spreadsheets/d/\"), url-prefix(\"https://docs.google.com/spreadsheets/d/\"), url-prefix(\"http://docs.google.com/spreadsheet/\"), url-prefix(\"https://docs.google.com/spreadsheet/\")",
 	"/*[[page_bgrnd_color]]*/"      => "#333333",
 	"/*[[normal_text_color]]*/"     => "#DDDDDD",
 	"/*[[caret_border_color]]*/"    => "#CC9922",
