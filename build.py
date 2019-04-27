c0ce37cc6e4a4956b43ad340f31165fffb1acbbe
Mateen Ulhaq <mulhaq2005@gmail.com>
2019-04-26 17:25:53 -0700

    Build

--- build.py
+++ build.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
+import argparse
+import datetime
 import os
 
-replacements = [
+IN_FILE = 'userstyles.user.css'
+OUT_FILE = 'stylish.user.css'
+
+REPLACEMENTS = [
     ('/*[[theme_drive]]*/',         ('domain("drive.google.com"), '
                                      'url-prefix("http://docs.google.com/document/u/"), '
                                      'url-prefix("https://docs.google.com/document/u/"), '
@@ -26,12 +31,38 @@ replacements = [
     ('/*[[home_list_color]]*/',     '#333333'),
 ]
 
-with open('userstyles.user.css', 'r') as f_in, \
-     open('stylish.user.css',    'w') as f_out:
+def build():
+    print(f'Building on {datetime.datetime.now()}...')
+    with open(IN_FILE,  'r') as f_in, \
+         open(OUT_FILE, 'w') as f_out:
+        for line in f_in:
+            for s, r in REPLACEMENTS:
+                line = line.replace(s, r)
+            f_out.write(line)
 
-    for line in f_in:
-        for s, r in replacements:
-            line = line.replace(s, r)
+def main():
+    parser = argparse.ArgumentParser(description='Build userstyle.')
+    parser.add_argument('-m', '--monitor', action='store_true')
+    args = parser.parse_args()
 
-        f_out.write(line)
+    build()
 
+    if args.monitor:
+        import pyinotify
+
+        class ModificationHandler(pyinotify.ProcessEvent):
+            def my_init(self, watch_list):
+                self.watch_list = watch_list
+
+            def process_IN_MODIFY(self, event):
+                path = os.path.basename(event.pathname)
+                if any(f == path for f in self.watch_list):
+                    build()
+
+        handler = ModificationHandler(watch_list=[IN_FILE])
+        wm = pyinotify.WatchManager()
+        notifier = pyinotify.Notifier(wm, handler)
+        wdd = wm.add_watch(os.getcwd(), pyinotify.IN_MODIFY)
+        notifier.loop()
+
+main()
